afd0a9ec830027cb9c51003a7e1def81cce3f8e6
Amir Kalaki <115138312+KLX1899@users.noreply.github.com>
2023-05-10 15:45:21 +0330

    Second commit
    I imported a the biggest number finder in kadane() and its reason is now the program can support arrays that filled completely by negative numbers. also the program support another wrong inputs (floats input) with using a "temp" variable.

--- p6q2.c
+++ p6q2.c
@@ -8,26 +8,21 @@
 #include <stdio.h>
 
 
-int reader(int inputarr[] , int length) {
-
-    for (int i = 0 ; i < length ; i++) {
-        printf("Enter %dth number: " , (i+1));
-        scanf("%d" , &inputarr[i]);
-    }
-    return inputarr;
-}
-
-
 int kadane(int inputarr[] , int length) {
-    int maxSum = 0;
+    int maxSum = inputarr[0];
     int currentSum = 0;
 
     for(int i = 0 ; i < length ; i++) {
+        if (inputarr[i] > maxSum) {
+            maxSum = inputarr[i];
+        }
+    }
+    for(int i = 0 ; i < length ; i++) {
         currentSum = currentSum + inputarr[i];
-        /*if (currentSum < 0) {
+        if (currentSum < 0) {
             currentSum = 0;
-        }*/
-        if(maxSum <= currentSum) {
+        }
+        if(maxSum < currentSum) {
             maxSum = currentSum; 
         }
     }
@@ -38,11 +33,21 @@ int kadane(int inputarr[] , int length) {
 int main() {
 
     int length;
+    float temp;
     int inputarr[length];
 
     printf("Enter length of array: ");
-    scanf("%d" , &length);
-    reader(inputarr , length);
+    scanf("%f" , &temp);
+    length = (int)temp;
+    if (length < 0) {
+        printf("Invalid input. Please enter a positive integer.\n");
+        return 0;    
+    }
+    for (int i = 0 ; i < length ; i++) {
+        printf("Enter %dth number: " , (i+1));
+        scanf("%d" , &inputarr[i]);
+    }
+    //printf("%d , %d , %d" , inputarr[0] , inputarr[1] , inputarr[2]);
     printf("Maximum sum of any subarray is %d\n", kadane(inputarr , length));
     return 0;
 }
